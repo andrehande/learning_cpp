725788e1baae85e35458556c1158e6659749b25f
André H. Eggehagen <andre.hande@hotmail.com>
2019-09-23 11:57:21 +0200

    Added a logging class

---
+++ Logging/Logging/src/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class Log
+{
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	int m_LogLevel = LogLevelWarning;
+public:
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= LogLevelError)
+			std::cout << "[ERROR]: " << message << std::endl;
+	}
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+			std::cout << "[WARN]: " << message << std::endl;
+	}
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+			std::cout << "[INFO]: " << message << std::endl;
+	}
+};
+
+int main()
+{
+	Log log;
+	log.Warn("Hello world!");
+
+}
